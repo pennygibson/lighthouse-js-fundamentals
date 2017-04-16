6f6d6ee1f38697c333d6de38e5a93f2576e55dc3
pennygibson <cenneboulet@yahoo.ca>
2017-04-16 16:46:18 +0000

    adds for, while and reverseexercises

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+ var counter = 0;
+while
+  (counter < ingredients.length) {
+  counter++;
+  console.log(ingredients[counter]) }
+
+ for (var i = 0; i < ingredients.length; i++ ) {
+  console.log(ingredients[i]);
+}
+
+for (var i = 7; i > 0; i--) {
+  console.log(ingredients[i]);
+}
